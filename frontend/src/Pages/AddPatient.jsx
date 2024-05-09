c22e5dcdaba3396bf9a6144c2c8e1e54dcee45ab
LethalZoro <120679724+LethalZoro@users.noreply.github.com>
2024-05-09 09:41:31 +0500

    add Patient Search done

--- frontend/src/Pages/AddPatient.jsx
+++ frontend/src/Pages/AddPatient.jsx
@@ -17,10 +17,31 @@ const AddPatient = ({Side_panel,setSide_panel}) => {
   const[diagnosis,setDiagnosis] = useState('')
   const[amount,setAmount] = useState('')
   const [currentPage, setCurrentPage] = useState(1);
-const [itemsPerPage, setItemsPerPage] = useState(5);
+const [itemsPerPage, setItemsPerPage] = useState(10);
+const [searchTerm, setSearchTerm] = useState('');
+
+// const nextPage = () => {
+//   setCurrentPage(prevPageNumber => prevPageNumber + 1);
+// };
+const handleSearchChange = (event) => {
+  setSearchTerm(event.target.value);
+};
+// const prevPage = () => {
+//   setCurrentPage(prevPageNumber => prevPageNumber - 1);
+// };
+const filteredPatients = patients.filter(patient =>
+  Object.values(patient).some(value =>
+    value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+  )
+);
 const indexOfLastItem = currentPage * itemsPerPage;
 const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-const currentItems = patients.slice(indexOfFirstItem, indexOfLastItem);
+const currentItems = filteredPatients.slice(indexOfFirstItem, indexOfLastItem);
+
+const handleItemsChange = (event) => {
+  setItemsPerPage(Number(event.target.value));
+  setCurrentPage(1); // Reset current page to 1 when items per page changes
+};
 
   // const addPatient = (event) => {
   //   event.preventDefault();
@@ -130,9 +151,37 @@ const currentItems = patients.slice(indexOfFirstItem, indexOfLastItem);
         </div>
         <br />
         <div className="container">
+          
           <div className="table-container">
+
+          
           <br />
-        <h2 >Patients History </h2>
+          <div className="header-container">
+              <div className='show-entries'>
+              <label>Show entries: 
+                <select value={itemsPerPage} onChange={handleItemsChange}>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                </select>
+              </label>
+            </div>
+            <h2 >Patients History </h2>
+            {/* <div className='page-button'>
+                <button className="pagination-button" onClick={prevPage} disabled={currentPage === 1}>Previous</button>
+                <button className="pagination-button" onClick={nextPage} disabled={currentPage === Math.ceil(patients.length / itemsPerPage)}>Next</button>
+              </div> */}
+              <div className='page-button'>
+                {/* <label htmlFor="Search">Search:  </label>
+          <input type="text" placeholder="Search..." value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value);handleSearchChange}} /> */}
+                  <div className="group">
+          <svg className="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+          <input placeholder="Search" type="search" className="input-search" value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value);handleSearchChange}}/>
+        </div>
+            </div>
+          </div>
+        
             <table className='table'>
               <thead>
                 <tr>
@@ -147,7 +196,7 @@ const currentItems = patients.slice(indexOfFirstItem, indexOfLastItem);
                 </tr>
               </thead>
               <tbody>
-                {patients.map((patient, index) => (
+                {currentItems.map((patient, index) => (
                   <tr key={patient._id}>
                     <td >{index + 1}</td>
                     <td>{patient.name}</td>
@@ -174,6 +223,7 @@ const currentItems = patients.slice(indexOfFirstItem, indexOfLastItem);
       </div>
         </div>
         
+
         </div>
         <ToastContainer
           position="bottom-center"
