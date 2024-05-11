e8692d6bf6e59e2b94dc5400c54302154bec4d3a
LethalZoro <120679724+LethalZoro@users.noreply.github.com>
2024-05-11 08:39:31 +0500

    minor changes to dashboard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,12 +45,6 @@ const Home = ({Side_panel,setSide_panel}) => {
         <p className="title-text">
             Expenses
         </p>
-        <p className="percent">
-           <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
-                </path>
-            </svg> 20%
-        </p>
     </div>
     <div className="data">
         <p>
@@ -73,12 +67,7 @@ const IncomeCard = <>
       <p className="title-text">
           Income
       </p>
-      <p className="percent">
-         <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
-              </path>
-          </svg> 20%
-      </p>
+      
   </div>
   <div className="data">
       <p>
@@ -102,12 +91,7 @@ const ProfitCard = <>
       <p className="title-text">
           Cash in Hand
       </p>
-      <p className="percent">
-         <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
-              </path>
-          </svg> 60%
-      </p>
+      
   </div>
   <div className="data">
       <p>
